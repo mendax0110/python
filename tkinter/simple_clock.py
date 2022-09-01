113f27f0bca610b9e0259b8a690872fbcc66e48e
m3ndax <adrian.goessl@outlook.com>
2022-09-01 20:05:30 +0200

    update code and comment

---
+++ tkinter/simple_clock.py
@@ -0,0 +1,22 @@
+#import the modules
+import sys
+from tkinter import *
+import time
+
+def times():
+    CURRENT_TIME = time.strftime("%H:%M:%S")
+    CLOCK.config(text=CURRENT_TIME)
+    CLOCK.after(200, times)
+
+root = Tk()
+root.geometry("500x250")
+CLOCK=Label(root, font=("times", 50, 'bold'), bg='green')
+CLOCK.grid(row=2, column=2, pady=25, padx=100)
+times()
+
+digi = Label(root, text="Digital Clock", font=("times", 24, 'bold'))
+digi.grid(row=0, column=2)
+
+nota = Label(root, text="hours   minutes   seconds   ", font="times 15 bold")
+nota.grid(row=3, column=2)
+root.mainloop()
