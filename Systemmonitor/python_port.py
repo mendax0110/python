45cdc8076a68ddd4d283473a5c66ee76f15edf2b
m3ndax <adrian.goessl@outlook.com>
2022-09-07 18:22:05 +0200

    update code and comment

---
+++ Systemmonitor/python_port.py
@@ -0,0 +1,16 @@
+#import the packages
+import psutil
+
+def python_port_name(process_name, port):
+    processes = [proc for proc in psutil.process_iter() 
+        if proc.name() == process_name]
+
+    for p in processes:
+        for c in p.connections():
+            if c.status == 'LISTEN ON PORT' and c.ladder.port == port:
+                return p
+
+    return None
+
+#print the result
+print("PID", " ")
