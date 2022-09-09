94dc86ed0273b220f487636b8e21c23845bc7ab6
m3ndax <adrian.goessl@outlook.com>
2022-09-09 16:52:38 +0200

    update code and comment

---
+++ serial/simple_UART.py
@@ -0,0 +1,16 @@
+#import the libraries
+import mraa
+import time
+import sys
+
+#set the serial port
+port = "/dev/ttyMFD1"
+
+#set value to send
+data = 'IMPUT YOUR DATA HERE'
+
+#initialize the serial port
+uart = mraa.Uart(port)
+
+#send the data to the serial port
+uart.write(bytearray(data, 'utf-8'))
