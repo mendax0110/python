cbc44a77a5dbf9b641271322d75e6af3f1b0028a
m3ndax <adrian.goessl@outlook.com>
2022-09-26 19:59:22 +0200

    update code and comment

--- binary/hex_2_dec.py
+++ binary/hex_2_dec.py
@@ -1,25 +1,25 @@
-#import the necessary modules
+#import the sys module
 import sys
 
 l = {10:'A', 11:'B', 12:'C', 13:'D', 14:'E', 15:'F'}
 
-dec = int(input("Enter a decimal number: "))
+dec = int(input("Enter the decimal number: "))
 
 c = " "
 
 while dec > 0:
-    
+
     a = dec % 16
 
     if a <= 15 and a > 9:
-        c += str(l[a])
+        c = l[a] + c
     else:
-        c += str(a)
+        c = str(a) + c
     dec = dec // 16
 
     #print the result
     print("The hexadecimal number is: ", c[::-1])
-    
+
     #exit the program
     sys.exit()
-    
+
