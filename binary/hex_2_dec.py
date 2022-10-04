9cfa766d247d55a7eb7e3064d926a2d8d23932b3
m3ndax <adrian.goessl@outlook.com>
2022-10-04 19:16:35 +0200

    update comment

--- binary/hex_2_dec.py
+++ binary/hex_2_dec.py
@@ -3,10 +3,12 @@ import sys
 
 l = {10:'A', 11:'B', 12:'C', 13:'D', 14:'E', 15:'F'}
 
+#get the decimal value from the user
 dec = int(input("Enter the decimal number: "))
 
 c = " "
 
+#convert the decimal number to hex
 while dec > 0:
 
     a = dec % 16
