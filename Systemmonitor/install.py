c746b78ea36d8e000e9659f9407e41ffdb50a4f9
m3ndax <adrian.goessl@outlook.com>
2022-10-05 19:04:18 +0200

    update code and comment

---
+++ Systemmonitor/install.py
@@ -0,0 +1,33 @@
+#import the required modules
+import sys
+import subprocess
+
+#ask which moduke to install
+print("Which module do you want to install?")
+#get the input
+module = input("Module: ")
+
+#instll the module
+subprocess.call([sys.executable, "-m", "pip", "install", module])
+
+# check if the user wants to install another module
+while True:
+    # ask the user if he wants to install another module
+    install_another_module = input("Do you want to install another module? (y/n): ")
+    # if the user wants to install another module
+    if install_another_module == "y":
+        # ask the user which module he wants to install
+        module_to_install = input("Which module do you want to install? (name): ")
+        # install the module
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', module_to_install])
+    # if the user doesn't want to install another module
+    elif install_another_module == "n":
+        # exit the program
+        sys.exit()
+    # if the user enters something else
+    else:
+        # ask the user again
+        continue
+
+# exit the program
+sys.exit()
