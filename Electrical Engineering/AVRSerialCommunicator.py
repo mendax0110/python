489345ad3b7cbf61c690720b3308a2779ea5b8a7
m3ndax <adrian.goessl@outlook.com>
2022-12-25 16:04:11 +0100

    change the error handling, fixed bugs

--- Electrical Engineering/AVRSerialCommunicator.py
+++ Electrical Engineering/AVRSerialCommunicator.py
@@ -15,22 +15,31 @@ baud_field = tk.Entry(window)
 
 # Create the port selection field
 port_var = tk.StringVar(window)
-port_field = tk.OptionMenu(window, port_var, *[port.device for port in list_ports.comports()])
+port_options = [port.device for port in list_ports.comports()]
+if not port_options:
+    port_options = ['No available ports']
+port_field = tk.OptionMenu(window, port_var, *port_options)
 
 # Create the send button
 def send_data():
     # Read the data from the input field and write it to the serial port
     data = input_field.get()
-    ser.write(data.encode())
-    input_field.delete(0, tk.END)
+    if ser and ser.is_open:
+        ser.write(data.encode())
+        input_field.delete(0, tk.END)
+    else:
+        output_field.insert(tk.END, 'Error: Not connected to a serial port\n')
 
 send_button = tk.Button(window, text="Send", command=send_data)
 
 # Create the receive button
 def receive_data():
     # Read data from the serial port and display it in the output field
-    data = ser.readline()
-    output_field.insert(tk.END, data.decode())
+    if ser and ser.is_open:
+        data = ser.readline()
+        output_field.insert(tk.END, data.decode())
+    else:
+        output_field.insert(tk.END, 'Error: Not connected to a serial port\n')
 
 receive_button = tk.Button(window, text="Receive", command=receive_data)
 
@@ -38,14 +47,17 @@ receive_button = tk.Button(window, text="Receive", command=receive_data)
 def connect():
     # Set up the serial port using the values entered by the user
     global ser
-    ser = serial.Serial(
-        port=port_var.get(),
-        baudrate=int(baud_field.get()),
-        parity=serial.PARITY_NONE,
-        stopbits=serial.STOPBITS_ONE,
-        bytesize=serial.EIGHTBITS,
-        timeout=1
-    )
+    try:
+        ser = serial.Serial(
+            port=port_var.get(),
+            baudrate=int(baud_field.get()),
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.EIGHTBITS,
+            timeout=1
+        )
+    except serial.serialutil.SerialException:
+        output_field.insert(tk.END, 'Error: Unable to connect to specified port\n')
 
 connect_button = tk.Button(window, text="Connect", command=connect)
 
