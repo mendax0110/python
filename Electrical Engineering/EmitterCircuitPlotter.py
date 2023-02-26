f357bd16c989d95ffcf702300d3d919313a87fee
Adrian <52537419+mendax0110@users.noreply.github.com>
2023-02-26 09:19:11 +0100

    added some transistor changes

--- Electrical Engineering/EmitterCircuitPlotter.py
+++ Electrical Engineering/EmitterCircuitPlotter.py
@@ -5,7 +5,7 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 import numpy as np
 
-#
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -54,14 +54,34 @@ class MainWindow(QMainWindow):
             QtWidgets.QMessageBox.critical(self, "Error", "Invalid frequency value")
             return
 
+        # Set up circuit components
+        Rb = 10e3  # Base resistor
+        Rc = 1e3  # Collector resistor
+        Re = 470  # Emitter resistor
+        Vcc = 5  # Supply voltage
+        Vbe = 0.7  # Base-emitter voltage drop
+        hfe = 100  # Transistor current gain
+
         # Create data for plot
-        x = np.linspace(0, 1, 100)
-        y = amplitude * np.sin(2 * np.pi * frequency * x)
+        x = np.linspace(0, 1, 1000)
+        v_in = amplitude * np.sin(2 * np.pi * frequency * x)
+        v_out = np.zeros_like(x)
+
+        # Calculate output waveform
+        for i in range(len(x)):
+            # Calculate transistor bias and DC operating point
+            Ic = (Vcc - Vbe) / (Rb + (hfe + 1) * Re)
+            Vce = Vcc - Ic * Rc
+            Ve = Vce - Vbe
+
+            # Calculate AC voltage gain and output waveform
+            gm = Ic / Ve
+            v_out[i] = -gm * v_in[i] * Re / (1 + gm * (Rc + Re))
 
         # Clear old plot and create new one
         self.figure.clear()
         ax = self.figure.add_subplot(111)
-        ax.plot(x, y)
+        ax.plot(x, v_out)
 
         # Update plot widget
         self.canvas.draw()
