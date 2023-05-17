455a4bd0f31be76b192ce3f682b6e495f7adec04
Adrian <52537419+mendax0110@users.noreply.github.com>
2023-05-17 21:39:13 +0200

    open, run, automate Logisim Simulations

--- Electrical Engineering/RCFilter.py
+++ Electrical Engineering/RCFilter.py
@@ -1,24 +1,30 @@
+import tkinter
 import matplotlib.pyplot as plt
 import numpy as np
 from tkinter import *
+from tkinter import messagebox
 
 
 # Define the function for calculating and plotting the filter's frequency response
 def calculate_and_plot(R, C):
-    # Calculate the filter's corner frequency
-    fc = 1 / (2 * np.pi * R * C)
+    try:
+        # Calculate the filter's corner frequency
+        fc = 1 / (2 * np.pi * R * C)
 
-    # Define the frequency range for the plot
-    f = np.logspace(0, 9, 1000)
+        # Define the frequency range for the plot
+        f = np.logspace(0, 9, 1000)
 
-    # Calculate the filter's transfer function
-    H = 1 / (1 + (1j * f) / fc)
+        # Calculate the filter's transfer function
+        H = 1 / (1 + (1j * f) / fc)
 
-    # Plot the frequency response
-    plt.semilogx(f, 20 * np.log10(np.abs(H)))
-    plt.xlabel("Frequency (Hz)")
-    plt.ylabel("Gain (dB)")
-    plt.show()
+        # Plot the frequency response
+        plt.semilogx(f, 20 * np.log10(np.abs(H)))
+        plt.xlabel("Frequency (Hz)")
+        plt.ylabel("Gain (dB)")
+        plt.show()
+    except (ValueError, tkinter.TclError):
+        messagebox.showerror("Error",
+                             "Invalid input. Please enter valid numeric values for resistance and capacitance.")
 
 
 # Create the main window
@@ -42,5 +48,6 @@ capacitance_label.grid(row=1, column=0)
 capacitance_entry.grid(row=1, column=1)
 button.grid(row=2, column=0, columnspan=2)
 
-# Start the main loop
-root.mainloop()
+if __name__ == '__main__':
+    # Start the main loop
+    root.mainloop()
