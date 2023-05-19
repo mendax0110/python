5b39cf258658faa137b545e4e2e1e8aa389b1056
Adrian <52537419+mendax0110@users.noreply.github.com>
2023-05-19 14:14:34 +0200

    open, run, automate Logisim Simulations

--- Electrical Engineering/CMOSGraph.py
+++ Electrical Engineering/CMOSGraph.py
@@ -1,9 +1,14 @@
 import sys
+
+import matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QGridLayout, QLabel, QLineEdit, QPushButton
+matplotlib.use('QT5Agg')
 
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QGridLayout, QLabel, QLineEdit, \
+    QPushButton, QMessageBox
 
+#
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -33,21 +38,26 @@ class MainWindow(QMainWindow):
         # Set the layout of the widget
         self.setCentralWidget(self.widget)
 
+    # create a function to plot the graph
     def plot_graph(self):
-        # Get input values from text boxes
-        vdd = float(self.vdd_text.text())
-        vgs = np.linspace(0, float(self.vgs_text.text()), 1000)
+        try:
+            # Get input values from text boxes
+            vdd = float(self.vdd_text.text())
+            vgs = np.linspace(0, float(self.vgs_text.text()), 1000)
 
-        # Calculate output current
-        ids = np.where(vgs < 0, 0, np.where(vgs < (vdd - 0.5), 0.5 * vgs ** 2, (vdd - 0.25) * (vdd - vgs)))
+            # Calculate output current
+            ids = np.where(vgs < 0, 0, np.where(vgs < (vdd - 0.5), 0.5 * vgs ** 2, (vdd - 0.25) * (vdd - vgs)))
 
-        # Create a plot of the output characteristics
-        fig, ax = plt.subplots()
-        ax.plot(vgs, ids)
-        ax.set_xlabel("VGS (V)")
-        ax.set_ylabel("ID (A)")
-        ax.set_title("Output Characteristics of CMOS Transistor")
-        plt.show()
+            # Create a plot of the output characteristics
+            fig, ax = plt.subplots()
+            ax.plot(vgs, ids)
+            ax.set_xlabel("VGS (V)")
+            ax.set_ylabel("ID (A)")
+            ax.set_title("Output Characteristics of CMOS Transistor")
+            plt.show()
+        except ValueError as e:
+            error_message = f"Invalid input: {str(e)}"
+            QMessageBox.critical(self, "Error", error_message)
 
 
 if __name__ == "__main__":
